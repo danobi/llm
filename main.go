e0869aa0a21eb0c705613853f670e52e2d07bbbd
Daniel Xu <dxu@dxuuu.xyz>
2023-12-23 21:53:39 -0700

    Support reading key from filesystem

--- main.go
+++ main.go
@@ -6,15 +6,49 @@ import (
 	"io"
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/google/generative-ai-go/genai"
 	"google.golang.org/api/option"
 )
 
+// Get API key from well known locations
+func key() (string, error) {
+	if env := os.Getenv("API_KEY"); env != "" {
+		return env, nil
+	}
+
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return "", fmt.Errorf("failed to get home dir: %w", err)
+	}
+
+	fpath := filepath.Join(home, ".config/llm/key")
+	f, err := os.Open(fpath)
+	if err != nil {
+		return "", fmt.Errorf("failed to open key file: %w", err)
+	}
+	defer f.Close()
+
+	key, err := io.ReadAll(f)
+	if err != nil {
+		return "", fmt.Errorf("failed to read key file: %w", err)
+	}
+
+	return string(key), nil
+}
+
 func main() {
-	// Create client given API key
 	ctx := context.Background()
-	client, err := genai.NewClient(ctx, option.WithAPIKey(os.Getenv("API_KEY")))
+
+	// Get API key
+	k, err := key()
+	if err != nil {
+		log.Fatal("Failed to get API key: %v", err)
+	}
+
+	// Create client given API key
+	client, err := genai.NewClient(ctx, option.WithAPIKey(k))
 	if err != nil {
 		log.Fatal("Failed to create genai client: %v", err)
 	}
